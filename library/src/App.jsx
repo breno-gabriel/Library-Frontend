aba1546aa4051b843c2fec11c3b383c349272e59
Breno <brenomelogabriel2019@gmail.com>
2024-09-05 00:31:16 -0300

    feat: adding tailwindCSS

--- library/src/App.jsx
+++ library/src/App.jsx
@@ -1,10 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import Book from './components/Book';
+import "./App.css"
+
 
 function App() {
 
+  const [books, setBook] = useState([]);
+
+  const handleFetch = async () => {
+
+    try {
+      const response = await axios.get("http://localhost:3000/books");
+      setBook(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  useEffect(() => {
+
+    handleFetch();
+
+  }, []);
+
   return (
     <>
-      
+        
+      <h2 className= 'text-red-800 text-6xl'>Lista de livros</h2>
+
+      {
+        books.map((book) => 
+        
+          <Book book={book}/>
+
+        )
+      }
+
     </>
   )
 }
---
+++ library/src/components/Book.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Book = ({book}) => {
+  return (
+    <div>
+        
+        <div></div>
+
+        <div>
+            <h3></h3>
+            <p></p>
+            <p></p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Book
