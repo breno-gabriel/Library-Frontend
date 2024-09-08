1bec0ddfa992e9962843469718000c97b28ad815
Breno <brenomelogabriel2019@gmail.com>
2024-09-08 13:25:22 -0300

    fix: adjusting the main page route

--- library/src/main.jsx
+++ library/src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
     element: <App/>, 
     children: [
       {
-        path: '/purchase',
+        path: '/',
         element: <BookPurchasePage />
       },
       {
